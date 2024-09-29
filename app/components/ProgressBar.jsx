99f0a570a1807b7f8ae13de1ada8de0811cbaba1
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-09-29 22:59:11 +0530

    added .env

--- app/components/ProgressBar.jsx
+++ app/components/ProgressBar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ProgressBar = ({percentage}) => {
   return (
-    <div class="flex items-center p-2 justify-center">
-      <div class="w-full border rounded-lg shadow-inner bg-slate-50">
-        <div style={{width: `${percentage}%`}} class="rounded-lg h-10 bg-gradient-to-t from-green-400 to-green-500 shadow-md shadow-green-400/60">
+    <div className="flex items-center p-2 justify-center">
+      <div className="w-full border rounded-lg shadow-inner bg-slate-50">
+        <div style={{width: `${percentage}%`}} className="rounded-lg h-10 bg-gradient-to-t from-green-400 to-green-500 shadow-md shadow-green-400/60">
         </div>
       </div>
-      <div class="bg-white relative w-24 h-16 border-2 border-green-400 rounded-full shadow-xl shadow-green-400/30 -left-3">
-        <p class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-xl">{percentage}%</p>
+      <div className="bg-white relative w-24 h-16 border-2 border-green-400 rounded-full shadow-xl shadow-green-400/30 -left-3">
+        <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-xl">{percentage}%</p>
       </div>
     </div>
   )
--- app/server-actions.js
+++ app/server-actions.js
@@ -1,13 +1,13 @@
 "use server";
 
 export const getQuestion = async (questionNumber) => {
-  const res = await fetch(`http://localhost:3000/api/question?no=${questionNumber}`);
+  const res = await fetch(`${process.env.HOST_NAME}/api/question?no=${questionNumber}`);
   const data = await res.json();
   return data;
 };
 
 export const saveAndGetNextQuestion = async ({no, answers, seconds}) => {
-  const res = await fetch(`http://localhost:3000/api/question?no=${no}`, {
+  const res = await fetch(`${process.env.HOST_NAME}/api/question?no=${no}`, {
     method: `POST`,
     body: JSON.stringify({ no, answers, seconds }),
   });
